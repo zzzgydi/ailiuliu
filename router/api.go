c66270712c957eb73306951d2a417b64d33e0961
zzzgydi <zzzgydi@gmail.com>
2024-07-13 23:25:59 +0800

    feat: use auth middleware

--- router/api.go
+++ router/api.go
@@ -3,10 +3,13 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/zzzgydi/ailiuliu/router/controller"
+	"github.com/zzzgydi/ailiuliu/router/middleware"
 )
 
 func APIRouter(r *gin.Engine) {
 	api := r.Group("/api")
+
+	api.Use(middleware.AuthMiddleware())
 
 	api.GET("/model/list", controller.GetModelList)
 }
